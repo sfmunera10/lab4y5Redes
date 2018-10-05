ecd2636fabd2532e319aeebdf74a278035ec3c2f
Santiago Múnera <sf.munera10@uniandes.edu.co>
2018-10-05 17:03:06 -0500

    Concurrency Fix

--- src/tcp/TCPClient.java
+++ src/tcp/TCPClient.java
@@ -8,13 +8,12 @@ import java.security.MessageDigest;
 // Client class 
 public class TCPClient{ 
 	public static void main(String[] args) throws IOException{ 
+		// getting localhost ip 
+		InetAddress ip = InetAddress.getByName("localhost"); 
+		// establish the connection with server port 5056 
+		Socket s = new Socket(ip, 5056);
 		try
-		{  
-			// getting localhost ip 
-			InetAddress ip = InetAddress.getByName("localhost"); 
-
-			// establish the connection with server port 5056 
-			Socket s = new Socket(ip, 5056); 
+		{   
 			// obtaining input and out streams 
 			DataInputStream dis = new DataInputStream(s.getInputStream()); 
 			DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
@@ -29,33 +28,46 @@ public class TCPClient{
 				System.out.println("Receving file: "+filename);
 				String[] pathSeparator = filename.split("/");
 				filename = pathSeparator[pathSeparator.length-1];
-				File tmpDir = new File("C:/Users/Lenovo/Desktop/SEMESTRE 201820/Redes/LAB 4 y 5/TCP/client/"+filename);
-				while(tmpDir.exists()){
-					filename = s.getLocalPort() + filename;
-					tmpDir = new File("C:/Users/Lenovo/Desktop/SEMESTRE 201820/Redes/LAB 4 y 5/TCP/client/"+filename);
-				}
-				filename = "C:/Users/Lenovo/Desktop/SEMESTRE 201820/Redes/LAB 4 y 5/TCP/client/"+filename;
+				filename = "C:/Users/Lenovo/Desktop/SEMESTRE 201820/Redes/LAB 4 y 5/TCP/client/"+ s.getLocalPort() + filename;
 				System.out.println("Saving as file: "+filename);
+
+				BufferedInputStream bis = 
+						new BufferedInputStream(s.getInputStream());
+
+				BufferedOutputStream bos = 
+						new BufferedOutputStream(new FileOutputStream(filename));
+
 				long sz=Long.parseLong(dis.readUTF());
 				System.out.println ("File Size: "+(sz/(1024*1024))+" MB");
 
-				byte b[]=new byte [1024];
+				byte buffer[]=new byte [51200];
 				System.out.println("Receiving file..");
-				FileOutputStream fos=new FileOutputStream(new File(filename),true);
+				
+				
 				String md5Server = "", md5Client = "";
 				MessageDigest md = MessageDigest.getInstance("MD5");
 				int countErrors = 0;
-				long bytesRead = dis.read(b, 0, b.length);
-				while(!(bytesRead<1024)){
-					md5Server = dis.readUTF();
-					md.update(b, 0, b.length);
-					md5Client = new BigInteger(1, md.digest()).toString(16);
-					if(!md5Server.equals(md5Client)){
-						countErrors++;
-					}
-					fos.write(b,0,b.length);
-					bytesRead = dis.read(b, 0, b.length);
-				}
+				int len = 0;
+		        while ((len = bis.read(buffer)) > 0) {
+					md.update(buffer, 0, len);
+					md5Client += new BigInteger(1, md.digest()).toString(16) + ",";
+		            bos.write(buffer, 0, len);
+		        }
+		        md5Server = dis.readUTF();
+		        System.out.println("Checking file integrity with server...");
+		        String[] servMD5 = md5Server.split(",");
+		        String[] cliMD5 = md5Client.split(",");
+		        
+		        if(servMD5.length != cliMD5.length){
+		        	countErrors = 100;
+		        }
+		        
+		        for(int i = 0; i<servMD5.length; i++){
+		        	if(!servMD5[i].equals(cliMD5[i])){
+		        		countErrors++;
+		        	}
+		        }
+		        
 				if(countErrors > 1){
 					System.out.println("FILE INTEGRITY COMPROMISED. FILE MAY BE CORRUPTED.");
 				}
@@ -63,16 +75,18 @@ public class TCPClient{
 					System.out.println("SUCCESSFULL INTEGRITY VERIFICATION.");
 				}
 				System.out.println("Completed.");
-				fos.close();
+				bis.close();
+				bos.flush();
+				bos.close();
 				dis.close();
 				dos.close();
-				s.close();
 			}
 			catch(EOFException e)
 			{
 				//do nothing
 			}
-		}catch(Exception e){ 
+		}catch(Exception e){
+			s.close();
 			e.printStackTrace(); 
 		} 
 	} 
--- src/tcp/TCPServer.java
+++ src/tcp/TCPServer.java
@@ -3,7 +3,9 @@ package tcp;
 import java.io.*;
 import java.math.BigInteger;
 import java.net.*;
-import java.security.MessageDigest; 
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.concurrent.CyclicBarrier; 
 
 // Server class 
 public class TCPServer{ 
@@ -11,6 +13,7 @@ public class TCPServer{
 		// server is listening on port 5056 
 		ServerSocket ss = new ServerSocket(5056); 
 		String fileToSend = "";
+		int numClientsDesired = 0;
 		System.out.println("WELCOME TO A SIMPLE FILE TRANSFER THROUGH TCP");
 		String filename;
 		filename= "C:/Users/Lenovo/Desktop/SEMESTRE 201820/Redes/LAB 4 y 5/TCP/server/testl.wmv";
@@ -41,38 +44,96 @@ public class TCPServer{
 				System.out.println("Not a valid entry :(");
 			}
 		}
+		
+		BufferedReader br2=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("How many clients would you like to connect to this server?");
+		System.out.println("Type in the number of clients you would like: ");
+		String whichFile2 = "";
+
+		while (!whichFile2.equals("1") || !whichFile2.equals("2") || !whichFile2.equals("3") ||
+				!whichFile2.equals("4") || !whichFile2.equals("5") || !whichFile2.equals("10") ||
+				!whichFile2.equals("25")) {
+			whichFile2 = br2.readLine();
+			if(whichFile2.equals("1") || whichFile2.equals("2") || whichFile2.equals("3") ||
+					whichFile2.equals("4") || whichFile2.equals("5") 
+					|| whichFile2.equals("10") || whichFile2.equals("25")){
+				numClientsDesired = Integer.parseInt(whichFile2);
+				break;
+			} 
+			else{
+				System.out.println("Not a valid entry :(");
+			}
+		}
 
 		System.out.println("The file that will be sent is: " + fileToSend);
+
+		final CyclicBarrier gate = new CyclicBarrier(numClientsDesired+1);
 		// running infinite loop for getting 
 		// client request 
 		while (true)  
-		{ 
-			Socket s = null; 
-
+		{
+			System.out.println(numClientsDesired + " CLIENTS WILL BE INCLUDED IN THIS FILE TRANSFER.");
+			ArrayList<Socket> clients = new ArrayList<>();
+			ArrayList<Thread> threads = new ArrayList<>();
+			int numConnected = 0;
+			while(numConnected < numClientsDesired){
+				Socket s = null;
+				s = ss.accept();
+				if(s != null){
+					clients.add(s);
+					System.out.println("A new client is connected : " + s);
+					numConnected = clients.size();
+				}
+			}
 			try 
-			{ 
-				// socket object to receive incoming client requests 
-				s = ss.accept(); 
+			{
+				System.out.println("Assigning new thread for each new client...");
+				for(Socket so: clients){
+					// obtaining input and out streams 
+					DataInputStream dis = new DataInputStream(so.getInputStream()); 
+					DataOutputStream dos = new DataOutputStream(so.getOutputStream());
+					// create a new thread object
+					Thread t = new ClientHandler(so, dis, dos, fileToSend, gate);
+					threads.add(t);
+				}  
+				System.out.println("Number of threads ready: " + threads.size());
 
-				System.out.println("A new client is connected : " + s); 
+				for(Thread th: threads){
+					// Invoking the start() method 
+					th.start();
+				}
+				
+				BufferedReader br3=new BufferedReader(new InputStreamReader(System.in));
+				System.out.println();
+				System.out.println("Type Yes to start all threads: ");
+				String yas = "";
 
-				// obtaining input and out streams 
-				DataInputStream dis = new DataInputStream(s.getInputStream()); 
-				DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
-
-				System.out.println("Assigning new thread for this client"); 
-
-				// create a new thread object 
-				Thread t = new ClientHandler(s, dis, dos, fileToSend); 
-
-				// Invoking the start() method 
-				t.start();
+				while(!(yas.equals("Yes"))){
+					yas = br3.readLine();
+					if(yas.equals("Yes")){
+						gate.await();
+						break;
+					}
+					else{
+						System.out.println("Not a valid entry :(");
+					}
+				}
+				System.out.println("All threads started");
+				br.close();
+				
+				for (Thread thread : threads) {
+				    thread.join();
+				    System.out.println("Waiting for thread to finish...");
+				}
+				for(Socket so: clients){
+					so.close();
+					System.out.println("Done.");
+				}
 			} 
 			catch (Exception e){ 
 				ss.close();
-				s.close(); 
 				e.printStackTrace(); 
-			} 
+			}
 		}
 	} 
 } 
@@ -83,39 +144,46 @@ class ClientHandler extends Thread{
 	final DataOutputStream dos; 
 	final Socket s;
 	final String fileToSend;
-
+	final CyclicBarrier gate;
 
 	// Constructor 
-	public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos, String fileToSend)  
+	public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos, String fileToSend,
+			CyclicBarrier gate)  
 	{ 
 		this.s = s; 
 		this.dis = dis; 
 		this.dos = dos;
 		this.fileToSend = fileToSend;
-	} 
-
+		this.gate = gate;
+	}
+	
 	@Override
 	public void run()  
 	{
 		try{
+			gate.await();
 			System.out.println("Checking if client is ready...");
 			String str="";  
 
 			str=dis.readUTF();
-			
+
 			if(!str.equals("stop")){  
 				System.out.println("Sending File: "+fileToSend);
 				dos.writeUTF(fileToSend);  
 				dos.flush();  
 
+				BufferedInputStream bis = 
+			               new BufferedInputStream(
+			                    new FileInputStream(fileToSend));
+				BufferedOutputStream bos = 
+			               new BufferedOutputStream(s.getOutputStream());
+				
 				File f=new File(fileToSend);
 				System.out.println("Attempting to read from file in: "+f.getCanonicalPath());
-				FileInputStream fin=new FileInputStream(f);
+				
 				long sz=(int) f.length();
 
-				byte b[]=new byte [1024];
-
-				int read;
+				byte buffer[]=new byte [51200];
 
 				dos.writeUTF(Long.toString(sz)); 
 				dos.flush(); 
@@ -124,25 +192,22 @@ class ClientHandler extends Thread{
 				System.out.println ("Buf size: "+ s.getReceiveBufferSize());
 
 				MessageDigest md = MessageDigest.getInstance("MD5");
-
-				while((read = fin.read(b)) != -1){
-					dos.write(b, 0, read);
-					dos.flush();
-					md.update(b, 0, read);
-					String md5 = new BigInteger(1, md.digest()).toString(16);
-					dos.writeUTF(md5);
-					dos.flush();
-				}
+				String md5 = "";
+				int len = 0;
+		        while ((len = bis.read(buffer)) > 0){
+		        	md.update(buffer, 0, len);
+					md5 += new BigInteger(1, md.digest()).toString(16) + ",";
+		        	bos.write(buffer, 0, len);
+		        }
+		        System.out.println("Checking file integrity with client...");
+		        dos.writeUTF(md5);
+				bis.close();
+				bos.flush();
+				bos.close();
+				dos.close();
 				System.out.println("..ok");
-				System.out.println("Closing this connection."); 
-				this.s.close(); 
-				System.out.println("Connection closed");
-				dos.flush();
-				fin.close();
-			}  
-			System.out.println("Send Complete");
-
-			dos.flush();  
+				System.out.println("Send Complete");
+			}   
 		}
 		catch(Exception e)
 		{
